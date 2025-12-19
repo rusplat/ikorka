7fd834b7de2ba697a468714d04f416a0a23b0059
Esim <nsphspkp5j@privaterelay.appleid.com>
2025-12-19 18:10:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const products = [
     { id: 1, name: "Икра черная осетр (500гр)", price: 21000, oldPrice: 30000, img: "black.jpg.webp" },
     { id: 2, name: "Икра красная кета (500гр)", price: 4500, oldPrice: 6500, img: "red.jpg" },
     { id: 3, name: "Икра красная горбуша (500гр)", price: 3800, oldPrice: 5500, img: "red2.jpg" }
-    { id: 1, name: "Мясо краба (400гр)", price: 4890, oldPrice: 6990, img: "red3.jpg" },
+    { id: 4, name: "Мясо краба (400гр)", price: 4890, oldPrice: 6990, img: "red3.jpg" },
     ];
 
 // Корзина, загружаем из LocalStorage или создаем пустую
